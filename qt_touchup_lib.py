9f853f2b66b8ffa842d69ed3f78dee684a59e2ad
jryzkns <jryzkns@gmail.com>
2020-08-22 19:54:47 -0700

    code cleanup

--- qt_touchup_lib.py
+++ qt_touchup_lib.py
@@ -18,10 +18,10 @@ def render_qimage(pixdata, w, h):
         img.setPixel(i % w, i // w, qRgb(*pixelrgb))
     return img
 
-def get_trans_qimg(w, h):
-    return QImage(w, h, QImage.Format_ARGB32)
+def get_trans_qimage(w,h):
+    return QImage(w,h, QImage.Format_ARGB32)
 
-def clip(x, lo, hi):
+def clip(x,lo,hi):
     return lo if x <= lo else (hi if x > hi else x)
 
 def qimg2raws(qimg):
@@ -61,11 +61,11 @@ def touch_up(orig, mask, rad, mode):
         res = np.dstack((res, np.expand_dims(orig[:,:,3], axis=2)))
     return res
 
-def g_pt_mindist( group, pt,
-    # chebyshev distance
-    norm = lambda a, b, c, d: max(abs(a - c), abs(b - d))):
-    min_dist = norm(*pt, *group[0])
-    for pt_ in group[1:]:
+def c_pt_mindist( cluster, pt,
+    # 2D chebyshev/chessboard distance
+    norm = lambda a,b,c,d: max(abs(a-c),abs(b-d))):
+    min_dist = norm(*pt, *cluster[0])
+    for pt_ in cluster[1:]:
         dist = norm(*pt, *pt_)
         if dist < min_dist:
             min_dist = dist
@@ -80,9 +80,31 @@ def subsample2x(m):
 def get_mask_locs(m, th = 0.5):
     return list(np.argwhere(m > th).astype(np.int16))
 
-def reupscaled_group_bounds(g, w, h, padding = 20):
+def reupscaled_cluster_bounds(g, w, h, padding = 20):
     xs, ys = zip(*g)
     return  clip((min(xs) << 1) - padding, 0, w), \
             clip((max(xs) << 1) + padding, 0, w), \
             clip((min(ys) << 1) - padding, 0, h), \
             clip((max(ys) << 1) + padding, 0, h)
+
+def generate_mask_windows(m, th = 4):
+
+    subsampled_m = subsample2x(m) # subsample mask to save time
+    locs, mask_windows = get_mask_locs(subsampled_m), []
+    while len(locs) > 0:
+        # initialize a cluster by extracting a point from locs
+        cluster = [locs.pop()]
+        while True:
+            # generate a list of minimum distances from 
+            # points to current cluster, init remainder list
+            mi_dists = [c_pt_mindist(cluster, pt) for pt in locs]
+
+            # stop searching for this cluster if no more points left 
+            # or no more close points to cluster
+            if len(mi_dists) == 0 or min(mi_dists) > th:
+                break
+
+            cluster += [pt for i, pt in enumerate(locs) if mi_dists[i] <= th]
+            locs     = [pt for i, pt in enumerate(locs) if mi_dists[i] > th]
+        mask_windows.append(reupscaled_cluster_bounds(cluster, *m.shape))
+    return mask_windows
